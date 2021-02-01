28ae882229dcf71469ed35742f0d58ecb3ce0c1a
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-01-31 23:34:02 -0500

    Changed the project by making my own practice project. Did a do over of html, css. Used jQuery for filtering images

--- js/app.js
+++ js/app.js
@@ -1,31 +1,27 @@
-//when I click on the cakes button, only the cake images will filter out
-//I need an array to filter out. 
 
-//what do I need to get at? cakes, cupcakes, sweets and doughnuts
-// let all = document.getElementsByClassName('store-item');
-// let cakes = document.getElementsByClassName('cakes');
-// let cupcakes = document.getElementsByClassName('cupcakes');
-// let sweets = document.getElementsByClassName('sweets');
-// let doughnuts = document.getElementsByClassName('doughnuts');
-
-
-
-//buttons
-// let allButton = document.getElementById('all');
-// let cakesButton = document.getElementById('cakes');
-// let cupcakesButton = document.getElementById('cupcakes');
-// let sweetsButton = document.getElementById('sweets');
-// let doughnutsButton = document.getElementById('doughnuts');
-
-
-
-
-// function showCakes(){
-//     cupcakes.classList.add('hide');
-//     sweets.classList.add('hide');
-//     doughnuts.classList.add('hide');
-// };
-
-// cakesButton.addEventListener('click', showCakes);
-
-
+$(document).ready(function () {
+    $(".all").show();
+  
+    $("#cakes").on("click", () => {
+      $(".cakes").show();
+      $(".cupcakes").hide();
+      $(".sweets").hide();
+    });
+  
+    $("#cupcakes").on("click", () => {
+      $(".cupcakes").show();
+      $(".cakes").hide();
+      $(".sweets").hide();
+    });
+  
+    $("#sweets").on("click", () => {
+      $(".sweets").show();
+      $(".cakes").hide();
+      $(".cupcakes").hide();
+    });
+    
+    $('#all').on('click', () => {
+      $('.all').show();
+    })
+  });
+  
