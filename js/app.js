d81f6ee158236a3aaaa066a3fd44143730875a82
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-01-31 22:48:35 -0500

    First commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//when I click on the cakes button, only the cake images will filter out
+//I need an array to filter out. 
+
+//what do I need to get at? cakes, cupcakes, sweets and doughnuts
+// let all = document.getElementsByClassName('store-item');
+// let cakes = document.getElementsByClassName('cakes');
+// let cupcakes = document.getElementsByClassName('cupcakes');
+// let sweets = document.getElementsByClassName('sweets');
+// let doughnuts = document.getElementsByClassName('doughnuts');
+
+
+
+//buttons
+// let allButton = document.getElementById('all');
+// let cakesButton = document.getElementById('cakes');
+// let cupcakesButton = document.getElementById('cupcakes');
+// let sweetsButton = document.getElementById('sweets');
+// let doughnutsButton = document.getElementById('doughnuts');
+
+
+
+
+// function showCakes(){
+//     cupcakes.classList.add('hide');
+//     sweets.classList.add('hide');
+//     doughnuts.classList.add('hide');
+// };
+
+// cakesButton.addEventListener('click', showCakes);
+
+
